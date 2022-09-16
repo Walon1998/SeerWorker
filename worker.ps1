46f04b0dc54e01ff99cfb5a7d22bb1e1aba2d17d
Neville Walo <neville.walo@gmail.com>
2022-09-16 15:39:02 +0200

    updated worker.ps1

--- worker.ps1
+++ worker.ps1
@@ -1,9 +1,9 @@
 python --version
-#git pull
+git pull
 pip install --upgrade pip
 pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116
 pip install -r requirements.txt
 pip install -r requirements.txt --upgrade
-python RolloutWorker.py
+python RolloutWorker.py $args
 
 
