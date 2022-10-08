2a125d986485a0c35d1bc80b8eb88efd6bc0fe14
Neville Walo <neville.walo@gmail.com>
2022-10-08 21:33:52 +0200

    moved old model download to SeerPPO

--- Env/PastEnv.py
+++ Env/PastEnv.py
@@ -1,50 +1,20 @@
-import asyncio
-import glob
 import os
 import pickle
 import random
 import time
 from multiprocessing import Queue, Process
 
-import aiohttp
 import gym
 import numpy as np
 import requests
 import torch
-from tqdm import tqdm
+from SeerPPO.past_models_download import start_past_model_downloader
 
 from Env.MonitorWrapper import MonitorWrapper
 from Env.MulitEnv import MultiEnv
-from SeerPPO import SeerNetwork, RolloutBuffer
+from SeerPPO import SeerNetwork
 
 from Env.NormalizeReward import NormalizeReward
-
-
-async def download_models(session, url, disable=False):
-    local_files = [os.path.basename(x) for x in glob.glob("./Models/*.pt")]
-    async with session.get(url + "/pastmodels") as resp:
-        assert resp.status == 200
-        files = await resp.read()
-        files = files.decode('ascii').split(",")
-        file_to_get = set(files) - set(local_files)
-        for f in tqdm(file_to_get, disable=disable):
-            if f == '':
-                continue
-            async with session.get(url + "/Models/{f}".format(f=f)) as resp2:
-                assert resp2.status == 200
-                with open("./Models/{f}".format(f=f), "wb") as newfile:
-                    newfile.write(await resp2.read())
-
-
-async def past_model_downloader_async(url):
-    async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(total=0, connect=60, sock_connect=60, sock_read=60)) as session:
-        while True:
-            time.sleep(30)
-            await download_models(session, url, disable=True)
-
-
-def past_model_downloader(url):
-    asyncio.run(past_model_downloader_async(url))
 
 
 def get_past_models(session, url):
@@ -134,8 +104,7 @@ class PastEnv(gym.Env):
         p = Process(target=past_worker, args=(self.work_queue, self.result_queue, self.old_instances, device, url))
         p.start()
 
-        p = Process(target=past_model_downloader, args=(url,))
-        p.start()
+        start_past_model_downloader(url)
 
     def reset(self):
         obs = self.env.reset()
