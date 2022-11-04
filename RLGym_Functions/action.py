bac35df37ea6325f762e4fe99615921d2fb581e6
Neville Walo <neville.walo@gmail.com>
2022-11-04 22:53:25 +0100

    added v2

--- RLGym_Functions/action.py
+++ RLGym_Functions/action.py
@@ -6,8 +6,8 @@ from rlgym.utils.action_parsers import ActionParser
 from rlgym.utils.gamestates import GameState
 
 
-class SeerAction(ActionParser):
-    def __init__(self, flip_bins=5, throttle_bins=3, roll_bins=3):
+class SeerActionv2(ActionParser):
+    def __init__(self, flip_bins=3, throttle_bins=3, roll_bins=3):
         super(ActionParser).__init__()
         assert flip_bins % 2 == 1, "n_bins must be an odd number"
         assert throttle_bins % 2 == 1, "n_bins must be an odd number"
@@ -17,7 +17,7 @@ class SeerAction(ActionParser):
         self.throttle_bins = throttle_bins
         self.roll_bins = roll_bins
 
-        assert self.flip_bins == 5
+        assert self.flip_bins == 3
         assert self.throttle_bins == 3
         assert self.roll_bins == 3
 
@@ -27,12 +27,12 @@ class SeerAction(ActionParser):
 
     def parse_actions(self, actions: Any, state: GameState) -> np.ndarray:
         actions = actions.reshape((-1, 7)).astype(dtype=np.float32)
-        steer_yaw = actions[:, 1] * 0.5 - 1.0  # [0,1,2,3,4] -> [-1,-0.5,0,0.5,1]
+        steer_yaw = actions[:, 1] - 1.0  # [0,1,2] -> [-1,0,1]
 
         parsed = np.empty((actions.shape[0], 8), dtype=np.float32)
         parsed[:, 0] = actions[:, 0] - 1.0  # throttle [0,1,2] -> [-1,0,1]
         parsed[:, 1] = steer_yaw  # steer
-        parsed[:, 2] = actions[:, 2] * 0.5 - 1.0  # pitch [0,1,2,3,4] -> [-1,-0.5,0,0.5,1]
+        parsed[:, 2] = actions[:, 2] - 1.0  # pitch [0,1,2] -> [-1,0,1]
         parsed[:, 3] = steer_yaw  # yaw
         parsed[:, 4] = actions[:, 3] - 1.0  # roll [0,1,2] -> [-1,0,1]
         parsed[:, 5] = actions[:, 4]  # jump
