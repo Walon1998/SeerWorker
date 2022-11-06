fcfcb5c36d199259ce37522b88f03a078a18b672
Neville Walo <neville.walo@gmail.com>
2022-11-06 18:47:24 +0100

    improved replay_downloader.py

--- replay_downloader.ps1
+++
@@ -1,4 +0,0 @@
-for ($i=0; $i -le 49; $i=$i+1 ) {
-    Invoke-WebRequest http://nevillewalo.ch/assets/Replays_$i.zip -OutFile Replays_$i.zip
-    Expand-Archive -DestinationPath ".\Replays" Replays_$i.zip
-}
