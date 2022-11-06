b92dfdd65a10a980d1e085a4d1f462a34bf75a2c
Neville Walo <neville.walo@gmail.com>
2022-11-06 19:24:06 +0100

    fix

--- replay_downloader.py
+++ replay_downloader.py
@@ -7,6 +7,6 @@ from tqdm import tqdm
 if __name__ == '__main__':
     for dir in ["1v1", "2v2", "3v3"]:
         for i in tqdm(range(10)):
-            r = requests.get("http://nevillewalo.ch/assets/1v1_{i}.zip".format(i=i), stream=True)
+            r = requests.get("http://nevillewalo.ch/assets/{dir}_{i}.zip".format(dir=dir, i=i), stream=True)
             z = zipfile.ZipFile(io.BytesIO(r.content))
             z.extractall("./Replays")
