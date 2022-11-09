a4551b1aa3989e9b760f5f0b69e69fd4a21ec9ff
Neville Walo <43504521+Walon1998@users.noreply.github.com>
2022-11-10 00:02:56 +0100

    Update main.py

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ async def communication_worker_async_put(url, work_queue):
                 asyncio.ensure_future(send_rollout(session, url, rollout, rollout_version)),
                 asyncio.ensure_future(put_reward_mean_var(session, url, mean, var))]
 
-            asyncio.gather(*tasks)
+            await asyncio.gather(*tasks)
 
 
 def communication_worker_put(url, work_queue):
