3133a2bfb21ca0e6fd4ca8bc5c5ad404903792b1
Neville Walo <neville.walo@gmail.com>
2022-12-02 20:00:14 +0100

    0.3 distribute reward

--- Env/AsyncEnv.py
+++ Env/AsyncEnv.py
@@ -31,7 +31,7 @@ def worker(work_queue, result_queue, force_paging, team_size):
                      gravity=1,
                      boost_consumption=1,
                      terminal_conditions=[NoTouchTimeoutCondition(512), GoalScoredCondition()],
-                     reward_fn=AnnealRewards(DistributeRewardsV2(SeerRewardV2(), 0.3), DistributeRewardsV2(SeerRewardV2_1(), 0.3, 12_000, 15_000),
+                     reward_fn=AnnealRewards(DistributeRewardsV2(SeerRewardV2(), 0.3), DistributeRewardsV2(SeerRewardV2_1(), 0.3), 12_000, 15_000),
                      obs_builder=SeerObsV2(team_size),
                      action_parser=SeerActionV2(),
                      state_setter=WeightedSampleSetter(
