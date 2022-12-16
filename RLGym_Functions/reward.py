65ead3a64e8ec76c65359c356d8f12a7f9307659
Neville Walo <neville.walo@gmail.com>
2022-12-16 21:49:48 +0100

    fix

--- RLGym_Functions/reward.py
+++ RLGym_Functions/reward.py
@@ -149,9 +149,9 @@ class SeerRewardV2(RewardFunction):
                         AirReward()]
 
         self.weights = np.array([
-            1.25,  # Goal Scored, Sparse, {0,1-1.5}
+            1.0,  # Goal Scored, Sparse, {0,1-1.5}
             0.1,  # Boost, Sparse, [0,1]
-            0.05,  # Ball Touch, Sparse, [0,2]
+            0.025,  # Ball Touch, Sparse, [0,2]
             0.3,  # Demo, Sparse, {0,1}
             0.0025,  # Distance Ball Player, Cont., [0,1]
             0.0025,  # Distance Ball Goal, Cont., [0,1]
