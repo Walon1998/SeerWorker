0b54931035f6e95d75cb547bb76d4a351cfe5498
Neville Walo <neville.walo@gmail.com>
2023-01-03 23:27:07 +0100

    improved NormalizeReward.py

--- Env/NormalizeReward.py
+++ Env/NormalizeReward.py
@@ -12,38 +12,30 @@ spec = [
 
 @jitclass(spec)
 class RunningMeanStd:
-    def __init__(self, mean, var, epsilon=1e-4):
+    def __init__(self, mean, var):
         self.mean = mean
         self.var = var
-        self.count = epsilon
 
     def update(self, x):
         batch_mean = np.mean(x)
         batch_var = np.var(x)
-        batch_count = x.shape[0]
-        self.update_from_moments(batch_mean, batch_var, batch_count)
+        self.update_from_moments(batch_mean, batch_var)
 
-    def update_from_moments(self, batch_mean, batch_var, batch_count):
-        self.mean, self.var, self.count = update_mean_var_count_from_moments(
-            self.mean, self.var, self.count, batch_mean, batch_var, batch_count
+    def update_from_moments(self, batch_mean, batch_var):
+        self.mean, self.var,  = update_mean_var_count_from_moments(
+            self.mean, self.var,  batch_mean, batch_var
         )
+
 
 
 @jit(fastmath=True)
 def update_mean_var_count_from_moments(
-        mean, var, count, batch_mean, batch_var, batch_count
+        mean, var,  batch_mean, batch_var
 ):
-    delta = batch_mean - mean
-    tot_count = count + batch_count
-
-    new_mean = mean + delta * batch_count / tot_count
-    m_a = var * count
-    m_b = batch_var * batch_count
-    M2 = m_a + m_b + np.square(delta) * count * batch_count / tot_count
-    new_var = M2 / tot_count
-    new_count = tot_count
-
-    return new_mean, new_var, new_count
+    MOMENTUM = 0.001
+    new_mean = (1 - MOMENTUM) * mean + MOMENTUM * batch_mean
+    new_var = (1 - MOMENTUM) * var + MOMENTUM * batch_var
+    return new_mean, new_var
 
 
 class NormalizeReward(gym.core.Wrapper):
